f33cfd5546fc87493fc00565e56197d5c0c6d3b4
Gunashree C <55580701+Guna-777@users.noreply.github.com>
2020-10-05 18:45:37 +0530

    Add files via upload

---
+++ 4_gnomesort.py
@@ -0,0 +1,18 @@
+def gnomeSort( arr, n): 
+    index = 0
+    while index < n: 
+        if index == 0: 
+            index = index + 1
+        if arr[index] >= arr[index - 1]: 
+            index = index + 1
+        else: 
+            arr[index], arr[index-1] = arr[index-1], arr[index] 
+            index = index - 1
+  
+    return arr 
+arr = [ 18,24,-90,1] 
+n = len(arr) 
+arr = gnomeSort(arr, n) 
+print ("Sorted sequence after applying Gnome Sort :") 
+for i in arr: 
+    print (i)
